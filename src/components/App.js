a82102c96fcdf50a866af5dea134ca2f68417e75
ezure haruna <burimofumofu@gmail.com>
2021-03-16 12:04:55 +0900

    create-video-area

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,14 @@
 import React, { useState } from 'react';
 import InputFormLocal from './InputFormLocal';
 import InputFormRemote from './InputFormRemote';
+import VideoArea from './VideoArea';
 
 const App = () => {
 
   const [ localPeerName, setLocalPeerName ] = useState('');
   const [ remotePeerName, setRemotePeerName ] = useState('');
 
-  const getMedia =  async (state) => {
-    const constraints = { audio: true, video: true };
   
-    try {
-      return await navigator.mediaDevices.getUserMedia(constraints);
-      /* ストリームを使用 */
-    } catch(err) {
-      /* エラーを処理 */
-      console.log(err);
-    }
-  }
-  getMedia();
-
 
   return (
     <>
@@ -31,6 +20,10 @@ const App = () => {
         localPeerName={localPeerName}
         remotePeerName={remotePeerName}
         setRemotePeerName={setRemotePeerName}
+      />
+      <VideoArea
+        localPeerName={localPeerName}
+        remotePeerName={remotePeerName}
       />
    </>
   );
